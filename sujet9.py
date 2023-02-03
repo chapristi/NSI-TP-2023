5fc0b23be9948bf72607ef6f58249976027b2264
chapristi <70172942+chapristi@users.noreply.github.com>
2023-02-03 09:54:04 +0100

    Update sujet9.py

--- sujet9.py
+++ sujet9.py
@@ -2,6 +2,7 @@ def multiplication(n1,n2):
   if n2 == 0 or n1 == 0:
     return 0
   elif n1 < 0 and n2 < 0:
+    #surement mieux à faire ;)
     n1 -= n1 + n1
     n2 -= n2 + n2
     return n1  + multiplication(n1,n2-1)

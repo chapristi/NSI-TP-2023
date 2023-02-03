24c7c2631836ccff4b4b74764c461c5b4754adb3
chapristi <70172942+chapristi@users.noreply.github.com>
2023-02-03 09:53:35 +0100

    Create sujet9.py

---
+++ sujet9.py
@@ -0,0 +1,14 @@
+def multiplication(n1,n2):
+  if n2 == 0 or n1 == 0:
+    return 0
+  elif n1 < 0 and n2 < 0:
+    n1 -= n1 + n1
+    n2 -= n2 + n2
+    return n1  + multiplication(n1,n2-1)
+  else:
+    return n1  + multiplication(n1,n2-1)
+    
+print(multiplication(-4, -8))
+print(multiplication(3, 5))
+print(multiplication(-2, 0))
+print(multiplication(-2, 6))

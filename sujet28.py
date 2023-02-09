98aeaf0e09b2b42958011cdfd31edf94820a2c10
chapristi <70172942+chapristi@users.noreply.github.com>
2023-02-09 12:01:22 +0100

    Create sujet28.py

---
+++ sujet28.py
@@ -0,0 +1,37 @@
+def moyenne (tab):
+  note = 0
+  for i in tab:
+    note+=i
+  return float(note/len(tab))
+    
+  
+assert moyenne([1]) == 1
+assert moyenne([1, 2, 3, 4, 5, 6, 7]) == 4
+assert moyenne([1, 2]) == 1.5
+def dichotomie(tab, x):
+    """
+        tab : tableau triÃ© dans lâ€™ordre croissant
+        x : nombre entier
+        La fonction renvoie True si tab contient x et False sinon
+    """
+    # cas du tableau vide
+    if tab == []:
+        return False, 1
+    # cas oÃ¹ x n'est pas compris entre les valeurs extrÃªmes
+    if (x < tab[0]) or x > tab[-1]:
+        return False, 2
+    debut = 0
+    fin = len(tab) - 1
+    while debut <= fin:
+        m = (debut+fin)//2
+        if x == tab[m]:
+            return True
+        if x > tab[m]:
+            debut = m + 1
+        else:
+            fin = m -1
+    return False,3
+print(dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33], 28))
+print(dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33], 27))
+print(dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33], 1))
+print(dichotomie([], 28))

6448d463ca8341a03c5aea2ca378e69bc1a9f9a3
chapristi <70172942+chapristi@users.noreply.github.com>
2023-02-10 17:54:24 +0100

    Create sujet38.py

---
+++ sujet38.py
@@ -0,0 +1,37 @@
+def  correspond(mot,mot_a_trous):
+  if len(mot) != len(mot_a_trous):
+    return False
+  for i in range(len(mot)):
+    if mot[i] != mot_a_trous[i] and mot_a_trous[i] != "*":
+      return False
+  return True
+  
+
+print(correspond('INFORMATIQUE', 'INFO*MA*IQUE')) 
+print(correspond('AUTOMATIQUE', 'INFO*MA*IQUE'))
+print(correspond('STOP', 'S*'))
+print(correspond('AUTO', '*UT*'))
+def est_cyclique(plan):
+    '''
+    Prend en paramÃƒÂ¨tre un dictionnaire `plan` correspondant Ãƒ  un plan d'envoi de messages (ici entre les personnes A, B, C, D, E, F).
+    Renvoie True si le plan d'envoi de messages est cyclique et False sinon.
+    '''
+    expediteur = 'A'
+    destinataire = plan[expediteur]
+    nb_destinaires = 1
+    
+    while destinataire != expediteur:
+        destinataire = plan[destinataire]
+        nb_destinaires += 1
+
+    return nb_destinaires == len(plan)
+print(est_cyclique({'A':'E', 'F':'A', 'C':'D', 'E':'B', 'B':'F', 
+'D':'C'})
+)
+print(est_cyclique({'A':'E', 'F':'C', 'C':'D', 'E':'B', 'B':'F',
+'D':'A'}))
+print(est_cyclique({'A':'B', 'F':'C', 'C':'D', 'E':'A', 'B':'F', 
+'D':'E'}))
+print(est_cyclique({'A':'B', 'F':'A', 'C':'D', 'E':'C', 'B':'F', 
+'D':'E'})
+)

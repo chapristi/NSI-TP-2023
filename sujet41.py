1f73644f31a3d755d966650c17ca57be615bd3c5
chapristi <70172942+chapristi@users.noreply.github.com>
2023-02-10 18:43:00 +0100

    Update sujet41.py

--- sujet41.py
+++ sujet41.py
@@ -1,19 +1,24 @@
-def recherche(lettre,mot):
+
+def recherche(caractere,chaine):
   count = 0
-  for i in mot:
-    if lettre == i:
+  for i in chaine:
+    if i == caractere:
       count += 1
   return count
-print(recherche('e', "sciences"))
-print( recherche('i', "mississippi"))
+print( recherche('e', "sciences"))
+print(recherche('i', "mississippi")
+)
+print(recherche('a', "mississippi"))
 valeurs = [100,50,20,10,5,2,1]
 
 def rendu_glouton(a_rendre, rang):
     if a_rendre == 0:
         return []
     v = valeurs[rang]
-    if v <=  a_rendre:
+    if v <= a_rendre:
         return [v]+ rendu_glouton(a_rendre - v, rang)
     else :
-        return rendu_glouton(a_rendre, rang+1)
+        return rendu_glouton(a_rendre, rang +1)
 print(rendu_glouton(67, 0))
+print(rendu_glouton(291, 0))
+print(rendu_glouton(291,1) )
